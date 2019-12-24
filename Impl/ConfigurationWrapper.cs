650009215183a43536bef919ddd1c7ebcd842f3d
Hari Krishna Gaddipati <hari2m@gmail.com>
2019-12-23 20:10:12 -0600

    Initialized proj and added .gitignore file.

---
+++ Impl/ConfigurationWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExtensionWrapper.Interface;
+using Microsoft.Extensions.Configuration;
+
+namespace ExtensionWrapper.Impl
+{
+    public class ConfigurationWrapper : IConfigurationWrapper
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationWrapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetValue(string key)
+        {
+            return _configuration["key"];
+        }
+
+        public string GetConnectionString(string key)
+        {
+            return _configuration.GetConnectionString("key");
+        }
+    }
+}
---
+++ Interface/IConfigurationWrapper.cs
@@ -0,0 +1,8 @@
+﻿namespace ExtensionWrapper.Interface
+{
+    public interface IConfigurationWrapper
+    {
+        string GetValue(string key);
+        string GetConnectionString(string key);
+    }
+}
